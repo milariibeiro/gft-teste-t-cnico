94d8b0f38ba6c6d5b0071af33889b09eb59a564c
milariibeiro <104395797+milariibeiro@users.noreply.github.com>
2022-06-02 17:52:29 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-import class.Conta.java;
 
 public class ContaCorrente {
     public static void main(String[] args) {
  import class Conta {
-            int saldo = saldo++ 0.03;
+            int saldo = saldo + 0.03;
         System.out.println("seu saldo é:"+(saldo));
         }
     }
