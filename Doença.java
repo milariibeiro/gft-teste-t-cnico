1ec69523cb40d3fb159ccb83a55a568494741d52
milariibeiro <104395797+milariibeiro@users.noreply.github.com>
2022-06-02 18:09:44 -0300

    Add files via upload

---
+++ Doença.java
@@ -0,0 +1,10 @@
+
+public class Doença {
+    public static void main(String[] args) {
+        string Sintomas;
+        System.out.println("Insira seus sintomas: ");
+        if (Sintomas)
+            do System.out.println("Você tem:");
+
+    }
+}
