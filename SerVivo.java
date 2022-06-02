f7af02ed52d5a9c0d5b773b3ac20a225b4fdb304
milariibeiro <104395797+milariibeiro@users.noreply.github.com>
2022-06-02 18:05:06 -0300

    Add files via upload

---
+++ SerVivo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SerVivo {
+    public static void main(String[] args) {
+        Scanner SerVivo;
+        if (SerVivo = Vegetal);
+        while (System.out.println("CO2"));
+        while (System.out.println("O2"));
+    }
+}
+class Vegetal{
+    Scanner Vegeral;
+}
